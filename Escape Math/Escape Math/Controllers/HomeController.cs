fe0b07328960cf497835db8bfd164273d3611c02
Diego Rodriguez <tb.diego564@hotmail.com>
2021-10-22 20:23:34 -0500

    Se modifica HTML y se relaciona BD

--- Escape Math/Escape Math/Controllers/HomeController.cs
+++ Escape Math/Escape Math/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Escape_Math.Models;
+
 
 namespace Escape_Math.Controllers
 {
@@ -15,16 +17,27 @@ namespace Escape_Math.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult Personas()
+        {
+            using (var ctx = new EscapeMathEntities3())
+            {
+                var personas = ctx.tUsuarios.ToList();
+                return View(personas);
+            }
+
+            
+        }
+
     }
 }
