0844c029ccb1c4247f66c29d2455801c265b3426
Diego Rodriguez <tb.diego564@hotmail.com>
2021-10-28 23:32:03 -0500

    Se agrega el carrito de comprar por JavaScrip

---
+++ Escape Math/Escape Math/Scripts/carrito.js
@@ -0,0 +1,42 @@
+﻿class Carrito {
+    comprarProducto(e) {
+        e.preventDefault();
+        if (e.target.classList.contains('agregar-carrito')) {
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+    leerDatosProducto(producto) {
+        const infoProducto = {
+            imagen : producto.querySelector('img').src,
+            titulo : producto.querySelector('h4').textContent,
+            precio : producto.querySelector('.precio span').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad : 1
+        }
+        this.insertarCarrito(infoProducto);
+    }
+
+    insertarCarrito(producto) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${producto.imagen}" width=50> 
+            </td>
+            <td>${producto.titulo}</td>
+            <td>${producto.precio}</td>
+            <td>
+                <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+            </td>
+        `;
+        listaProductos.appendChild(row);
+    }
+
+    vaciarCarrito(e) {
+        e.preventDefault();
+        while (listaProductos.firstChild) {
+            listaProductos.removeChild(listaProductos.firstChild);
+        }
+        return false;
+        }
+    }
